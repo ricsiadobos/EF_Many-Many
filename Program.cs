7c7d7840919024d1ac8066702101c0f1c68f4a39
ricsiadobos <ricsirendel@gmail.com>
2022-09-14 07:02:25 +0200

    many-many

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using EF6.Data;
+using EF6.Domain;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EF6App
+{
+    class Program
+    {
+        private static FootballLeagueDbContext context = new FootballLeagueDbContext();
+        static async Task Main(string[] args)
+        {
+            var teams = context.Teams.ToList();
+            var CsakABayern = teams.Where(x=>x.Name.Contains("Bay")).ToList();
+            var NameSubstring =  teams.Select(x => x.Name.Substring(0,3)).ToList();
+        
+
+
+
+
+            Console.ReadLine();
+
+        }
+
+      
+    }
+}
